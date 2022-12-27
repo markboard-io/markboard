6e3f7f619ba5f53c3494970cf037be558ff15869
Shiqi Mei <shiqi.mei@lolimay.cn>
2022-12-27 19:41:24 +0800

    chore: introduce eslint into project to force code style

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'react/no-unescaped-entities': ['off'],
+    'react/jsx-no-target-blank': ['off']
+  }
+}
