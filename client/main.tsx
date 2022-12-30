808657abd03e4ad17a7f8b895d225ea5804e4a23
Shiqi Mei <shiqi.mei@lolimay.cn>
2022-12-30 14:04:38 +0800

    chore: update description meta tag

--- client/main.tsx
+++ client/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Meteor } from 'meteor/meteor'
 import { createRoot } from 'react-dom/client'
-import { router } from '../imports/pages/routes'
+import { router } from '/imports/pages/routes'
 import { RouterProvider } from 'react-router-dom'
 
 Meteor.startup(() => {
