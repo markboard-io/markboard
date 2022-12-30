6401fbd9369a607ca6428556469c70dcacf2223b
Shiqi Mei <shiqi.mei@lolimay.cn>
2022-12-31 01:50:26 +0800

    feat: protect route with authentication check and permission validation

--- imports/pages/routes.tsx
+++ imports/pages/routes.tsx
@@ -3,11 +3,38 @@ import { createBrowserRouter } from 'react-router-dom'
 import { Board } from './Board'
 import { Login } from './Login'
 import { NotFound } from './NotFound'
+import { Meteor } from 'meteor/meteor'
+import { Navigate } from 'react-router-dom'
+
+export interface IRouteRequirements {
+  needLogin: boolean
+  permissions: [] /* TODO: update it when role-based permission is introduced */
+}
+
+const withRouteProtected = (
+  Component: React.ComponentType,
+  requirements: IRouteRequirements = { needLogin: true, permissions: [] }
+) => {
+  const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const isLogged = Meteor.userId() != null
+
+    if (requirements.needLogin && !isLogged) {
+      return <Navigate to='/login' />
+    }
+    return <>{children}</>
+  }
+
+  return (
+    <ProtectedRoute>
+      <Component />
+    </ProtectedRoute>
+  )
+}
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Board />
+    element: withRouteProtected(Board)
   },
   {
     path: '/login',
