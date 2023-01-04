f9c019210a51ecc0d54bb01487d9bcf78d472916
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-01-04 22:28:21 +0800

    fix(explorer): resize handle active color behaviour not correct

--- imports/pages/Board/FileExplorerPanel/FileExplorerPanel.tsx
+++ imports/pages/Board/FileExplorerPanel/FileExplorerPanel.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Panel, PanelResizeHandle } from 'react-resizable-panels'
 import './FileExplorerPanel.style.scss'
+import { useEffect } from 'react'
 
 export function FileExplorerPanel() {
+  const [isDragging, setIsDragging] = useState(false)
+
+  const dividerStyle: React.CSSProperties = {
+    borderRight: isDragging ? '2px solid var(--background-primary)' : ''
+  }
+
+  useEffect(() => {
+    const onMouseUp = () => {
+      setIsDragging(false)
+    }
+    window.addEventListener('mouseup', onMouseUp)
+    return () => window.removeEventListener('mouseup', onMouseUp)
+  }, [])
+
   return (
     <>
       <Panel defaultSize={15} minSize={15} className='FileExplorerPanel'></Panel>
       <PanelResizeHandle className='PanelResizeHandle'>
-        <div className='Divider'></div>
+        <div className='Divider' style={dividerStyle} onMouseDown={() => setIsDragging(true)}></div>
       </PanelResizeHandle>
     </>
   )
