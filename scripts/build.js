d0d179e7d890ec14355203313a599956489b1ecf
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-01-14 19:42:09 +0800

    cd(docker): fix dockerfile steps

--- scripts/build.js
+++ scripts/build.js
@@ -2,5 +2,4 @@ const { sh } = require('./sh')
 
 sh('rm -rf dist')
 sh('meteor build --directory --server-only dist')
-sh('cd dist/bundle')
-sh('cd programs/server && npm install && cd -')
+sh('cd dist/bundle/programs/server && npm install')
