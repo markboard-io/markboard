a66aed3ba4593f348dca7dcf5476d73005b41705
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-01-15 01:59:27 +0800

    refactor(deps): remove fabricjs dependencies

--- imports/pages/Board/MainPanel/MainPanel.tsx
+++ imports/pages/Board/MainPanel/MainPanel.tsx
@@ -1,34 +1,9 @@
 import './MainPanel.style.scss'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Panel } from 'react-resizable-panels'
-import { fabric } from 'fabric'
 import { ExcalidrawApp } from '/imports/excalidraw'
 
 export function MainPanel() {
-  useEffect(() => {
-    const canvas = new fabric.Canvas('canvas-container', {
-      defaultCursor: 'default',
-      renderOnAddRemove: false,
-      imageSmoothingEnabled: false,
-      backgroundColor: '#F8F8F8',
-      skipOffscreen: true,
-      preserveObjectStacking: true,
-      selection: true,
-      fireRightClick: true
-    })
-    const resizeCanvasToFitWindow = () => {
-      canvas.setWidth(window.innerWidth)
-      canvas.setHeight(window.innerHeight)
-    }
-    resizeCanvasToFitWindow()
-    window.addEventListener('resize', resizeCanvasToFitWindow)
-    window.canvas = canvas
-
-    return () => {
-      window.removeEventListener('resize', resizeCanvasToFitWindow)
-    }
-  }, [])
-
   return (
     <Panel>
       <ExcalidrawApp />
