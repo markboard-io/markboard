2be3744124ba2406182578b3dbdb9dcf6039229f
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-01-14 17:54:43 +0800

    ci(do): add preinstall script instead of postinstall script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,5 +1,4 @@
 const fs = require('fs')
-const { sh } = require('sh')
 
 function setupDotEnvFile() {
   const isDotEnvFileExists = fs.existsSync('.env')
@@ -9,13 +8,4 @@ function setupDotEnvFile() {
   }
 }
 
-function deployOnDigitalOcean() {
-  sh('curl https://install.meteor.com/ | sh') // install meteor
-  sh('meteor npm install') // install dependencies via meteor
-  sh('rm package.json') // avoid digital ocean running `npm install`
-}
-
 setupDotEnvFile()
-if (process.env.DIGITAL_OCENAN != null) {
-  deployOnDigitalOcean()
-}
