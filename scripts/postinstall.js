4a6a237cf625724a0de12c93e93d098f5e4216e0
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-01-14 17:49:38 +0800

    ci(do): make some preinstall scripts

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,7 +1,21 @@
 const fs = require('fs')
+const { sh } = require('sh')
 
-const isDotEnvFileExists = fs.existsSync('.env')
+function setupDotEnvFile() {
+  const isDotEnvFileExists = fs.existsSync('.env')
 
-if (!isDotEnvFileExists) {
-  fs.copyFileSync('.env.example', '.env')
+  if (!isDotEnvFileExists) {
+    fs.copyFileSync('.env.example', '.env')
+  }
+}
+
+function deployOnDigitalOcean() {
+  sh('curl https://install.meteor.com/ | sh') // install meteor
+  sh('meteor npm install') // install dependencies via meteor
+  sh('rm package.json') // avoid digital ocean running `npm install`
+}
+
+setupDotEnvFile()
+if (process.env.DIGITAL_OCENAN != null) {
+  deployOnDigitalOcean()
 }
