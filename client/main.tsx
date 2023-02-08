2b504c0dbeb39dab38974f541481c413422a135d
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-02-08 11:29:23 +0800

    refactor: refactor client main.tsx to make it more concise

--- client/main.tsx
+++ client/main.tsx
@@ -13,22 +13,20 @@ import { jotaiStore } from '/imports/store/jotai'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-toastify/dist/ReactToastify.css'
 
-const Launcher = () => {
-  const DebugAtoms = () => {
-    useAtomsDebugValue()
-    return null
-  }
-
-  return (
-    <TopErrorBoundary>
-      <Provider unstable_createStore={() => jotaiStore}>
-        <DebugAtoms />
-        <RouterProvider router={router} />
-        <ToastContainer />
-      </Provider>
-    </TopErrorBoundary>
-  )
+const DebugAtoms = () => {
+  useAtomsDebugValue()
+  return null
 }
+
+const Launcher = () => (
+  <TopErrorBoundary>
+    <Provider unstable_createStore={() => jotaiStore}>
+      <DebugAtoms />
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </Provider>
+  </TopErrorBoundary>
+)
 
 Meteor.startup(() => {
   const root = document.getElementById('root') as HTMLDivElement
