3f35fb86304311fa0b1b27ea165ba5678e7e9323
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-02-09 22:25:09 +0800

    refactor(excalidraw/app): refactor localStorage

--- imports/excalidraw/app/data/localStorage.ts
+++ imports/excalidraw/app/data/localStorage.ts
@@ -5,29 +5,30 @@ import { clearElementsForLocalStorage } from '../../element'
 import { STORAGE_KEYS } from '../app_constants'
 import { ImportedDataState } from '../../data/types'
 
+/** Save a username to local storage */
 export const saveUsernameToLocalStorage = (username: string) => {
   try {
     localStorage.setItem(STORAGE_KEYS.LOCAL_STORAGE_COLLAB, JSON.stringify({ username }))
-  } catch (error: any) {
-    // Unable to access window.localStorage
+  } catch (error) {
     console.error(error)
   }
 }
 
+/** Import a username from local storage */
 export const importUsernameFromLocalStorage = (): string | null => {
   try {
     const data = localStorage.getItem(STORAGE_KEYS.LOCAL_STORAGE_COLLAB)
     if (data) {
       return JSON.parse(data).username
     }
-  } catch (error: any) {
-    // Unable to access localStorage
-    console.error(error)
+  } catch (error) {
+    console.error('Unable to access local storage', error)
   }
 
   return null
 }
 
+/** Import elements and app state from local storage */
 export const importFromLocalStorage = () => {
   let savedElements = null
   let savedState = null
@@ -35,18 +36,16 @@ export const importFromLocalStorage = () => {
   try {
     savedElements = localStorage.getItem(STORAGE_KEYS.LOCAL_STORAGE_ELEMENTS)
     savedState = localStorage.getItem(STORAGE_KEYS.LOCAL_STORAGE_APP_STATE)
-  } catch (error: any) {
-    // Unable to access localStorage
-    console.error(error)
+  } catch (error) {
+    console.error('Unable to access local storage', error)
   }
 
   let elements: ExcalidrawElement[] = []
   if (savedElements) {
     try {
       elements = clearElementsForLocalStorage(JSON.parse(savedElements))
-    } catch (error: any) {
+    } catch (error) {
       console.error(error)
-      // Do nothing because elements array is already empty
     }
   }
 
@@ -57,25 +56,25 @@ export const importFromLocalStorage = () => {
         ...getDefaultAppState(),
         ...clearAppStateForLocalStorage(JSON.parse(savedState) as Partial<AppState>)
       }
-    } catch (error: any) {
-      console.error(error)
-      // Do nothing because appState is already null
+    } catch (error) {
+      console.error('Unable to access local storage', error)
     }
   }
   return { elements, appState }
 }
 
+/** Get the storage size of elements */
 export const getElementsStorageSize = () => {
   try {
     const elements = localStorage.getItem(STORAGE_KEYS.LOCAL_STORAGE_ELEMENTS)
-    const elementsSize = elements?.length || 0
-    return elementsSize
-  } catch (error: any) {
-    console.error(error)
+    return elements?.length || 0
+  } catch (error) {
+    console.error('Unable to access local storage', error)
     return 0
   }
 }
 
+/** Get the total size of data stored in the local storage */
 export const getTotalStorageSize = () => {
   try {
     const appState = localStorage.getItem(STORAGE_KEYS.LOCAL_STORAGE_APP_STATE)
@@ -88,11 +87,12 @@ export const getTotalStorageSize = () => {
 
     return appStateSize + collabSize + librarySize + getElementsStorageSize()
   } catch (error: any) {
-    console.error(error)
+    console.error('Unable to access local storage', error)
     return 0
   }
 }
 
+/** Get the library items stored in the local storage */
 export const getLibraryItemsFromStorage = () => {
   try {
     const libraryItems: ImportedDataState['libraryItems'] = JSON.parse(
@@ -101,7 +101,7 @@ export const getLibraryItemsFromStorage = () => {
 
     return libraryItems || []
   } catch (error) {
-    console.error(error)
+    console.error('Unable to access local storage', error)
     return []
   }
 }
