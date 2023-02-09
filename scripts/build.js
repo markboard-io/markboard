4002fed716305a2d5b7e6980e7fb369c7bdaeb50
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-02-09 23:14:39 +0800

    ci(build): fix typo in build scripts

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,6 @@
 const { sh } = require('./sh')
 
 sh('rm -rf dist')
-sh('metoer remove bundle-visualizer')
+sh('meteor remove bundle-visualizer')
 sh('meteor build --directory --server-only dist')
 sh('cd dist/bundle/programs/server && npm install')
