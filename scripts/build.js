18394e3e6dff9fada4891dcc197853796495ef02
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-02-21 22:33:50 +0800

    ci(build): remove `meteor remove bundle-visualizer` from build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,5 @@
 const { sh } = require('./sh')
 
 sh('rm -rf dist')
-sh('meteor remove bundle-visualizer')
 sh('meteor build --directory --server-only dist')
 sh('cd dist/bundle/programs/server && npm install')
