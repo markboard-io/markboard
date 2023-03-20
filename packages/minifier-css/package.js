87cf72bc032fd1d835161203c7aae88359a9ef6b
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-03-20 17:23:34 +0800

    tash: introduce scss-modules support for the project

---
+++ packages/minifier-css/package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  name: 'markboard:minifier-css',
+  version: '1.10.0',
+  summary: 'Standard css minifier used with Meteor apps by default.'
+})
+
+Package.registerBuildPlugin({
+  name: 'minifyStdCSS',
+  use: ['minifier-css', 'ecmascript', 'logging'],
+  npmDependencies: {
+    '@babel/runtime': '7.18.9',
+    'source-map': '0.7.4',
+    'lru-cache': '6.0.0',
+    micromatch: '4.0.5'
+  },
+  sources: ['plugin/minify-css.js']
+})
+
+Package.onUse(function (api) {
+  api.use('minifier-css@1.5.4')
+  api.use('isobuild:minifier-plugin@1.0.0')
+  api.use('logging@1.3.1')
+})
