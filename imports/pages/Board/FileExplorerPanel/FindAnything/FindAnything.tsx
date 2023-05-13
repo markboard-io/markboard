238a198ecb204ada4717015a093e28c6dd63d493
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-05-13 21:12:11 +0800

    feat: add shortcuts support & improve the UI

--- imports/pages/Board/FileExplorerPanel/FindAnything/FindAnything.tsx
+++ imports/pages/Board/FileExplorerPanel/FindAnything/FindAnything.tsx
@@ -1,123 +1,152 @@
-import React, { useState, useRef, useEffect, MutableRefObject } from 'react'
+import React, { useState, useRef, useEffect, MutableRefObject, KeyboardEvent } from 'react'
 import { SearchIcon } from '/imports/components/icons'
 import styles from './FindAnything.module.scss'
 import { Services } from '/imports/services/client'
 import { useSwitchBoard } from '/imports/hooks'
 import { SearchResults } from '/imports/models'
+import { globalEventEmitter } from '/imports/utils'
+import { EVENT } from '/imports/excalidraw/constants'
+import { KEYS } from '/imports/excalidraw/keys'
+import cx from 'classnames'
 
 export function FindAnything() {
   const [isFindAnythingModalOpen, setIsFindAnythingModalOpen] = useState(false)
-  const [searchResults, setSearchResults] = useState<SearchResults>([])
-  const modalInputRef = useRef<HTMLInputElement>(null) as MutableRefObject<HTMLInputElement>
+  const [candidateBoards, setCandidateBoards] = useState<SearchResults>([])
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  const input$ = useRef<HTMLInputElement>(null) as MutableRefObject<HTMLInputElement>
   const switchBoard = useSwitchBoard()
 
   const toggleFindAnythingModal = () => {
     setIsFindAnythingModalOpen(!isFindAnythingModalOpen)
-    setSearchResults([])
-    console.log('toggleFindAnythingModal', isFindAnythingModalOpen)
+    setCandidateBoards([])
   }
 
   useEffect(() => {
     if (isFindAnythingModalOpen) {
-      modalInputRef.current.focus()
+      input$.current.focus()
     }
   }, [isFindAnythingModalOpen])
 
-  const searchAnything = async () => {
-    const searchTerm = modalInputRef.current.value
+  const handleShortcuts = (ev: Event) => {
+    const { ctrlKey, metaKey, key } = ev as unknown as KeyboardEvent
+
+    // `Meta + P` open search modal if modal isn't open
+    if (!isFindAnythingModalOpen && metaKey && key === 'p') {
+      ev.preventDefault()
+      setIsFindAnythingModalOpen(true)
+    }
+
+    // `Ctrl + P` to select previous item
+    if (isFindAnythingModalOpen && ctrlKey && key === 'p') {
+      ev.preventDefault()
+      setActiveIndex(index => Math.max(0, index - 1))
+    }
+
+    // `Ctrl + N` to select next item
+    if (isFindAnythingModalOpen && ctrlKey && key === 'n') {
+      ev.preventDefault()
+      setActiveIndex(index => Math.min(candidateBoards.length - 1, index + 1))
+    }
+
+    // `Escape` hide search modal if it's open
+    if (isFindAnythingModalOpen && key === KEYS.ESCAPE) {
+      setIsFindAnythingModalOpen(false)
+    }
+
+    // `Enter` to open selected board
+    if (isFindAnythingModalOpen && key === KEYS.ENTER) {
+      setIsFindAnythingModalOpen(false)
+      const activeBoard = candidateBoards[activeIndex]
+      if (activeBoard != null) {
+        setCandidateBoards([])
+        switchBoard(activeBoard.boardId)
+      }
+    }
+  }
+
+  useEffect(() => {
+    globalEventEmitter.on(EVENT.KEYDOWN, handleShortcuts)
+    return () => globalEventEmitter.off(EVENT.KEYDOWN, handleShortcuts)
+  }, [isFindAnythingModalOpen])
+
+  const searchBoards = async () => {
+    const searchTerm = input$.current.value
     const results = await Services.get('board').searchBoards(searchTerm)
-    setSearchResults(results)
+    setActiveIndex(0)
+    setCandidateBoards(results)
   }
 
   return (
-    <div>
+    <>
+      <div className={styles.FindAnything} onClick={toggleFindAnythingModal}>
+        {SearchIcon}
+        <div className={styles.placeholder}>Find Anything</div>
+      </div>
       {isFindAnythingModalOpen ? (
-        <div>
-          <div className={styles.FindAnything}>
+        <div className={styles.FindAnythingModal} onClick={toggleFindAnythingModal}>
+          <div className={cx(styles.SearchBox, candidateBoards.length > 0 && styles.hasResults)}>
             {SearchIcon}
-            <input onClick={() => toggleFindAnythingModal()} placeholder='Find Anything' />
+            <input
+              onKeyDown={ev => handleShortcuts(ev.nativeEvent)}
+              onInput={searchBoards}
+              ref={input$}
+              placeholder='Find Anything'
+            />
           </div>
-          <div className={styles.SearchContent}>
-            <div className={styles.Modal}>
-              <div className={styles.Modal_Content}>
-                {SearchIcon}
-                <input
-                  onClick={() => toggleFindAnythingModal()}
-                  onInput={searchAnything}
-                  ref={modalInputRef}
-                  placeholder='Find Anything'
-                />
-              </div>
-              {searchResults.length > 0 ? (
-                <>
-                  <div className={styles.SearchResults}>
-                    {searchResults.map((result, index) => {
-                      return (
-                        <div key={index} className={styles.SearchResult}>
-                          <div
-                            onClick={() => {
-                              switchBoard(result.boardId)
-                              toggleFindAnythingModal()
-                            }}
-                          >
-                            {result.titles
-                              .split(new RegExp(`(${modalInputRef.current.value})`, 'gi'))
-                              .map((part, index) => {
-                                if (
-                                  part.toLowerCase() === modalInputRef.current.value.toLowerCase()
-                                ) {
-                                  return (
-                                    <span key={index} style={{ fontWeight: 'bold' }}>
-                                      {part}
-                                    </span>
-                                  )
-                                } else {
-                                  return part
-                                }
-                              })}
-                          </div>
-                          <div className={styles.SearchResultTexts}>
-                            {result.validTexts.map((text, index) => {
-                              const parts = text.split(
-                                new RegExp(`(${modalInputRef.current.value})`, 'gi')
-                              )
-                              return (
-                                <div key={index} className={styles.SearchResultText}>
-                                  {parts.map((part, index) => {
-                                    if (
-                                      part.toLowerCase() ===
-                                      modalInputRef.current.value.toLowerCase()
-                                    ) {
-                                      return (
-                                        <span key={index} style={{ fontWeight: 'bold' }}>
-                                          {part}
-                                        </span>
-                                      )
-                                    } else {
-                                      return part
-                                    }
-                                  })}
-                                </div>
-                              )
+          {candidateBoards.length > 0 ? (
+            <div className={styles.SearchResults}>
+              {candidateBoards.map((result, index) => {
+                return (
+                  <div
+                    key={index}
+                    className={cx(styles.SearchResult, index === activeIndex && styles.active)}
+                  >
+                    <div onClick={() => switchBoard(result.boardId)}>
+                      {result.titles
+                        .split(new RegExp(`(${input$.current.value})`, 'gi'))
+                        .map((part, index) => {
+                          const isMatch = part.toLowerCase() === input$.current.value.toLowerCase()
+                          if (isMatch) {
+                            return (
+                              <span key={index} style={{ fontWeight: 'bold' }}>
+                                {part}
+                              </span>
+                            )
+                          } else {
+                            return part
+                          }
+                        })}
+                    </div>
+                    <div className={styles.SearchResultTexts}>
+                      {result.validTexts.map((text, index) => {
+                        const parts = text.split(new RegExp(`(${input$.current.value})`, 'gi'))
+                        return (
+                          <div key={index} className={styles.SearchResultText}>
+                            {parts.map((part, index) => {
+                              if (part.toLowerCase() === input$.current.value.toLowerCase()) {
+                                return (
+                                  <span key={index} style={{ fontWeight: 'bold' }}>
+                                    {part}
+                                  </span>
+                                )
+                              } else {
+                                return part
+                              }
                             })}
                           </div>
-                        </div>
-                      )
-                    })}
+                        )
+                      })}
+                    </div>
                   </div>
-                </>
-              ) : (
-                []
-              )}
+                )
+              })}
             </div>
-          </div>
+          ) : (
+            []
+          )}
         </div>
-      ) : (
-        <div className={styles.FindAnything}>
-          {SearchIcon}
-          <input onClick={() => toggleFindAnythingModal()} placeholder='Find Anything' />
-        </div>
-      )}
-    </div>
+      ) : null}
+    </>
   )
 }
