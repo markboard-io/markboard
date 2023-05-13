bd6cc2b300595b2e0775203b175a599920425938
Shiqi Mei <shiqi.mei@lolimay.cn>
2023-05-13 21:45:06 +0800

    build: switch Node.js to 14.21 before build

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,11 @@
 const { sh } = require('./sh')
 
+// switch to Node.js 14.21
+sh('eval "`fnm env`"')
+sh('fnm use 14.21')
+sh('node --version')
+
+// build the project
 sh('rm -rf dist')
 sh('meteor build --directory --server-only dist')
 sh('cd dist/bundle/programs/server && npm install')
